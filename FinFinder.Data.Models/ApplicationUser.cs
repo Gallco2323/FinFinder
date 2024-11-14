dffbce2514179374501721bea2930886307fe3f1
Gallco2323 <125117450+Gallco2323@users.noreply.github.com>
2024-11-14 17:03:13 +0200

    Added entities and application user

---
+++ FinFinder.Data.Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinFinder.Data.Models
+{
+    public class ApplicationUser : IdentityUser<Guid> // Specify Guid as the primary key type
+    {
+        public string ProfilePictureURL { get; set; }
+        public string Bio { get; set; }
+
+        // Navigation Properties
+        public ICollection<FishCatch> FishCatches { get; set; }
+        public ICollection<Favorite> Favorites { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+        public ICollection<Observation> Observations { get; set; }
+    }
+}
