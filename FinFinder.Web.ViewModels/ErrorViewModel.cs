fba6f0249991ca7b41ae2bfc55f3820ec5451814
Gallco2323 <125117450+Gallco2323@users.noreply.github.com>
2024-11-15 00:44:24 +0200

    Add .gitignore to exclude .vs folder

--- FinFinder.Web.ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FinFinder.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
