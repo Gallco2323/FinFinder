1868513a5db7ca40a8daf16f902228f5c1e1e1b0
Gallco2323 <125117450+Gallco2323@users.noreply.github.com>
2024-11-27 12:12:39 +0200

    Added custom AutoMapper

--- FinFinder/Common/obj/Debug/net8.0/FinFinder.Common.AssemblyInfo.cs
+++ FinFinder/Common/obj/Debug/net8.0/FinFinder.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinFinder.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f32d3a6fe3fbfc24a796ec790029d78a690f64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinFinder.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinFinder.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FinFinder/FinFinder.Services.Data/obj/Debug/net8.0/FinFinder.Services.Data.AssemblyInfo.cs
+++ FinFinder/FinFinder.Services.Data/obj/Debug/net8.0/FinFinder.Services.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinFinder.Services.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f32d3a6fe3fbfc24a796ec790029d78a690f64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinFinder.Services.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinFinder.Services.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
