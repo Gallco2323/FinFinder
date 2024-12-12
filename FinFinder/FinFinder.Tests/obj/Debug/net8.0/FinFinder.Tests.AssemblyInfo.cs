2f7e814d933fead5c4b25e187bdc997fbb17ea93
Gallco2323 <125117450+Gallco2323@users.noreply.github.com>
2024-12-12 17:07:57 +0200

    FinalCommit

--- FinFinder/FinFinder.Tests/obj/Debug/net8.0/FinFinder.Tests.AssemblyInfo.cs
+++ FinFinder/FinFinder.Tests/obj/Debug/net8.0/FinFinder.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinFinder.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f32d3a6fe3fbfc24a796ec790029d78a690f64")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinFinder.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinFinder.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
